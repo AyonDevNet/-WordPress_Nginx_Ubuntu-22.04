15aa310a3aedf8a551e60d0c10ce1c58cf15911f
AyonDevNet <rmcoder7@gmail.com>
2025-06-13 17:00:03 +0600

    add

--- data.sql
+++ data.sql
@@ -3,3 +3,14 @@ CREATE USER 'wpuser'@'localhost' IDENTIFIED BY '********';
 GRANT ALL PRIVILEGES ON wordpress * TO 'wpuser';
 FLUSH PRIVILEGES;
 EXIT;
+
+
+/** Update DB settings: */
+define( 'DB_NAME', 'wordpress_db' );
+define( 'DB_USER', 'wpuser' );
+define( 'DB_PASSWORD', 'StrongPassword123!' );
+define( 'DB_HOST', 'localhost' );
+/**Optional: generate salts here  https://api.wordpress.org/secret-key/1.1/salt/ */
+
+
+
--- script.sh
+++ script.sh
@@ -3,22 +3,43 @@
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install nginx mysql-server php-fpm php-mysql unzip curl -y
+sudo apt install mysql-server -y
+sudo mysql_secure_installation
+
 sudo mysql -u root -p
 
 #Download WordPress
-cd /etc
+cd /tmp
 curl -O https://wordpress.org/latest.tar.gz
 tar -xvzf latest.tar.gz
-sudo mv wordpress /var/www/mycoolblog
 
+#Copy to /var/www/:
+
+sudo mkdir -p /var/www/ #(websites name)
+sudo cp -a wordpress/. /var/www/ #(websites name)
 
 
 #Set Permissions
-sudo chown -R www-data:www-data /var/www/mycoolblog
-sudo chmod -R 755 /var/www/mycoolblog
+sudo chown -R www-data:www-data /var/www/ #(websites name)mycoolblog
+sudo chmod -R 755 /var/www/
 
-#Create a new site config:
-sudo nano /etc/nginx/sites-available/mycoolblog
+sudo chown -R www-data:www-data /var/www/ #(websites name)
+sudo find /var/www/#(websites name) -type d -exec chmod 755 {} \;
+sudo find /var/www/#(websites name) -type f -exec chmod 644 {} \;
+
+
+#Create WordPress Config:
+cd /var/www/ #(websites name)
+cp wp-config-sample.php wp-config.php
+nano wp-config.php
+
+
+# Step 6: Configure Nginx
+#Create server block:
+
+sudo nano /etc/nginx/sites-available/ayonxyz.com
+
+
 
 #Then enable the config:
 
@@ -29,4 +50,10 @@ sudo systemctl reload nginx
 #install SSL 
 
 sudo apt install certbot python3-certbot-nginx -y
-sudo certbot --nginx -d mycoolblog.com -d www.mycoolblog.com
+sudo certbot --nginx -d adddomain-name-without-www -d adddomain-name with www 
+
+
+## must be allow your ips from your host machine, like, /etc/hosts, 
+#where you have to give name and ip of your machines
+#And also whenever you purchase the domain you have to see the dns config, like where you have to put your ip address.
+ 
